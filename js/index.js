abce5b8c50337db6360f3f0eb4d06b06a2cfa0e7
XENONDK <dch3005@gmail.com>
2023-02-14 09:20:02 +0100

    init commit

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // slå dig løs her... 
 
+    
+    const createImg = document.createElement("img")
+    createImg.src = hero.image
+    
+    const heroSection = document.querySelector(".hero")
+
+    heroSection.appendChild(createImg)
+
+    const createContainer = document.createElement("div")
+    const headingOne = document.createElement("h1")
+    const heroText = document.createElement("p")
+
+    createContainer.appendChild(headingOne)
+    headingOne.textContent = hero.headline
+
+    createContainer.appendChild(heroText)
+    heroText.textContent = hero.copy
+
+    heroSection.appendChild(createContainer)
+
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
 
