45593231684518a7d49d571f9109303210b619bc
XENONDK <dch3005@gmail.com>
2023-02-17 16:51:50 +0100

    finish

--- js/data.js
+++ js/data.js
@@ -19,7 +19,7 @@ let services = [
         headline: "Additional services designed for the needs of campers",
         text: "Travelling often means that you dont have time to organise excursions along your route. EasyCamper takes care of that!",
         linktext: "Discover the Guided Tours and additional services that you can link to your structure"
-    }, 
+    },
     {
         illustration: "https://dazzling-squirrel-77a63d.netlify.app/imgs/illustrations/destination.png",
         headline: "Free online showcase with promotion in Italy and abroad",
@@ -30,7 +30,7 @@ let services = [
 
 let facilities = {
     headline: "Making your facility known is our priority",
-    options : [
+    options: [
         {
             icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/list-check.png",
             headline: "Choose the type of stop",
@@ -79,20 +79,47 @@ let sites = {
 
 let advantages = [
     {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/compare.png",
+        icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/compare.png",
         headline: "Convenience and Transparency",
         text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
     },
     {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/library_books.png",
+        icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/library_books.png",
         headline: "Digital marketing and editorial support",
         text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
     },
     {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Compare-HSV.png",
+        icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Compare-HSV.png",
         headline: "Community of services dedicated to campers",
         text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
     },
 ]
 
 // Opret dit dataobjekt til footerens indhold herunder
+
+let footer = {
+    headline: "Easy Camper",
+    slogan: "When Passion Meets Comfort.",
+    footerNavigation: [
+        {
+            title: "Discover the Network",
+            textOne: "Where to shop",
+            textTwo: "Tour Guided",
+            textThree: "Our Tours",
+            textFour: "About Us"
+        },
+        {
+            title: "Become a Manager",
+            textOne: "join your Pairing",
+            textTwo: "Offer your  Service",
+            textThree: "Are you a guide?"
+        },
+        {
+            title: "Assistance",
+            textOne: "Support Center",
+            textTwo: "FAQs",
+            textThree: "Cancallation Options",
+            textFour: "Reliabilty and Sequrity"
+        }
+    ]
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,191 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     // slå dig løs her... 
 
+    // hero start
+    function heroFunction() {
+        const createImg = document.createElement("img")
+        createImg.src = hero.image
+
+        const heroSection = document.querySelector(".hero")
+
+        heroSection.appendChild(createImg)
+
+        const createContainer = document.createElement("div")
+        const headingOne = document.createElement("h1")
+        const heroText = document.createElement("p")
+
+        createContainer.appendChild(headingOne)
+        headingOne.textContent = hero.headline
+
+        createContainer.appendChild(heroText)
+        heroText.textContent = hero.copy
+
+        heroSection.appendChild(createContainer)
+
+    }
+    heroFunction()
+    // hero end
+
+
+    // service start
+    services.forEach(element => {
+        let container = document.createElement("div")
+        let title = document.createElement("h1")
+        let hyperLink = document.createElement("a")
+        let serviceP = document.createElement("p")
+        let serviceImg = document.createElement("img")
+
+        serviceImg.src = element.illustration
+        title.textContent = element.headline
+        hyperLink.href = "#"
+        // spørg lukas hvad #! betyder igen
+        hyperLink.innerHTML = element.linktext
+        serviceP.textContent = element.text
+
+        container.appendChild(serviceImg)
+        container.appendChild(title)
+        container.appendChild(serviceP)
+        container.appendChild(hyperLink)
+        document.querySelector(".services").appendChild(container)
+    })
+    // service end
+
+
+    // facilities start
+    let facilitiesHeadline = document.createElement("h2")
+    facilitiesHeadline.textContent = facilities.headline
+    document.querySelector(".facilities").appendChild(facilitiesHeadline)
+
+    facilities.options.forEach(element => {
+        let container = document.createElement("div")
+        let title = document.createElement("h1")
+        let facilitiesP = document.createElement("p")
+        let FacilitiesImg = document.createElement("img")
+
+        FacilitiesImg.src = element.icon
+        title.textContent = element.headline
+        facilitiesP.textContent = element.text
+
+
+
+
+        container.appendChild(FacilitiesImg)
+        container.appendChild(title)
+        container.appendChild(facilitiesP)
+
+        document.querySelector(".facilities").appendChild(container)
+    })
+    // facilities end
+
+
+    // sites start
+    function siteFunction() {
+        let OuterContainer = document.createElement("div")
+        let OuterTitle = document.createElement("h1")
+        let OuterP = document.createElement("p")
+        let outerImg = document.createElement("img")
+
+        OuterTitle.textContent = sites.headline
+        OuterP.textContent = sites.text
+        outerImg.src = sites.btnicon
+
+        OuterContainer.appendChild(OuterTitle)
+        OuterContainer.appendChild(OuterP)
+        OuterContainer.appendChild(outerImg)
+        document.querySelector(".sites").appendChild(OuterContainer)
+
+        sites.places.forEach(element => {
+            let Container = document.createElement("div")
+            let Title = document.createElement("h1")
+            let city = document.createElement("p")
+            let imgCity = document.createElement("img")
+
+            imgCity.src = element.img
+            Title.textContent = element.name
+            city.textContent = element.city
+
+
+            Container.appendChild(imgCity)
+            Container.appendChild(Title)
+            Container.appendChild(city)
+            document.querySelector(".sites").appendChild(Container)
+        })
+    }
+    siteFunction()
+    // sites end
+
+
+    // advantages start
+    advantages.forEach(element => {
+        let Container = document.createElement("div")
+        let Title = document.createElement("h1")
+        let paragraph = document.createElement("p")
+        let img = document.createElement("img")
+
+        img.src = element.icon
+        Title.textContent = element.headline
+        paragraph.textContent = element.text
+
+
+        Container.appendChild(img)
+        Container.appendChild(Title)
+        Container.appendChild(paragraph)
+        document.querySelector(".advantages").appendChild(Container)
+    })
+
+    // advantages end
+    function footerFunction() {
+        let footerCreation = document.createElement("footer")
+        footerCreation.setAttribute("class", "footer")
+        document.body.appendChild(footerCreation)
+        
+        let OuterContainer = document.createElement("div")
+        let OuterTitle = document.createElement("h1")
+        let OuterP = document.createElement("p")
+        
+
+
+        OuterTitle.textContent = footer.headline
+        OuterP.textContent = footer.slogan
+        
+
+        OuterContainer.appendChild(OuterTitle)
+        OuterContainer.appendChild(OuterP)
+        
+        document.querySelector("footer").appendChild(OuterContainer)
+
+        footer.footerNavigation.forEach(element => {
+            let Container = document.createElement("div")
+            let Title = document.createElement("h1")
+            let firstLine = document.createElement("p")
+            let secondLine = document.createElement("p")
+            let thirdLine = document.createElement("p")
+            let fourthLine = document.createElement("p")
+            
+            Title.textContent = element.title
+            firstLine.textContent = element.textOne 
+            secondLine.textContent = element.textTwo
+            thirdLine.textContent = element.textThree
+            fourthLine.textContent = element.textFour
+
+            Container.appendChild(Title)
+            Container.appendChild(firstLine)
+            Container.appendChild(secondLine)
+            Container.appendChild(thirdLine)
+            Container.appendChild(fourthLine)
+            
+          
+            document.querySelector(".footer").appendChild(Container)
+        })
+    }
+
+    
+    footerFunction()
+    // footer start
+
+
+    // footer end
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
 
